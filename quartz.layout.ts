ab5d19f029a310193bb0994fcb6eb6fc355a1a25
alfaelitery <yaumi.alfadha@elitery.com>
2025-09-03 16:58:59 +0700

    remove explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    //Component.Explorer(),
   ],
   right: [
     Component.Graph(),
